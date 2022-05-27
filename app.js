4c4923e19d2b79033cf1732bf25a5f7d0428f9af
HrnTrkNl <demirisaev16@gmail.com>
2022-05-27 13:35:51 +0200

    new update

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const result = document.getElementById('result');
 const category = document.getElementById('category');
 const reset = document.querySelector("#reset");
 const idealkilo = document.querySelector(".idealkilo");
+const bmr = document.querySelector('#bmr');
+const ageInput = document.querySelector('#age');
+const diet = document.querySelector('.diet');
+const idealtarget = document.querySelector('.idealkilo_target');
+const eatdaily = document.querySelector('.eatdaily');
+const dietweek = document.querySelector('.dietweek');
 //let height = parseInt(heightCm);
 //et weight = parseInt(weightKg);
 
@@ -21,27 +27,43 @@ function eventListeners(){
 function CalculateBMI(e) {
     let weight = weightKg.value.trim();
     let height = heightCm.value.trim();
+    let age = ageInput.value.trim();
     const idealbmi = 22.5;
-
-    
+   
+        
+    let bmrCal = Math.round((10 * weight) + (6.25 * height) - (5 * age)); 
     let BMI = Math.round(weight / (height/100 * height/100));
     let idealKilo = Math.round(idealbmi * (height/100 * height/100));
+    
+   
+    let overCalcweight = Math.round(weight - idealKilo); 
+   
+    let dailyCaloris = Math.round (bmrCal * 1.4 );
+    let dietWeeks = Math.round(overCalcweight / 0.5) ;
+    let dietCaloris = Math.round(bmrCal - 500) ;
+
    
    
-    Result(BMI, idealKilo);
+    Result(BMI, idealKilo, bmrCal, dietWeeks, dietCaloris, dailyCaloris);
   
     
      
    e.preventDefault();
    weightKg.value = "";
    heightCm.value = "";
+   ageInput.value = "";
 }
 
-function Result(BMI, idealKilo){
+function Result(BMI, idealKilo, bmrCal, dietWeeks, dietCaloris, dailyCaloris){
     if (BMI > 1){
         display.style.display = "block"; 
+        diet.style.display = "block";
     }
-    idealkilo.textContent =  `Your ideal kilo is ${idealKilo}`;
+    idealkilo.textContent =  `Your ideal weight is ${idealKilo}`;
+    bmr.textContent = `With a normal lifestyle you burn ${bmrCal} calories a day`;
+    idealtarget.textContent = `If you want to reach your ideal weight of ${idealKilo} kg: `;
+    eatdaily.textContent = `Eat calories  ${dietCaloris} a day`;
+    dietweek.textContent = `For ${dietWeeks} weeks`;
     result.textContent = BMI ;
     if (BMI < 18.5) {
 
